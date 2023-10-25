210b0aa3aade8f6c55be84ed84abbaee4bbeb815
Przemysław Wojtasik <przemyslaw.wojtasik@erlang-solutions.com>
2023-10-26 00:27:12 +0200

    Display game board sprites

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,22 +16,22 @@
 //
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html"
+import 'phoenix_html'
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
-import topbar from "../vendor/topbar"
-import Canvas from "./canvas.js"
+import { Socket } from 'phoenix'
+import { LiveSocket } from 'phoenix_live_view'
+import topbar from '../vendor/topbar'
+import Canvas from './canvas.js'
 
-let Hooks = {Canvas: Canvas.Hook}
+const Hooks = { Canvas: Canvas.Hook }
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {hooks: Hooks, params: {_csrf_token: csrfToken}})
+const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute('content')
+const liveSocket = new LiveSocket('/live', Socket, { hooks: Hooks, params: { _csrf_token: csrfToken } })
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
-window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
+topbar.config({ barColors: { 0: '#29d' }, shadowColor: 'rgba(0, 0, 0, .3)' })
+window.addEventListener('phx:page-loading-start', () => topbar.show(300))
+window.addEventListener('phx:page-loading-stop', () => topbar.hide())
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -1,38 +1,105 @@
-import { Application, Sprite, Assets } from 'pixi.js';
+import { Application, Sprite, Container, Texture, Rectangle } from 'pixi.js'
+
+const boardPath = '/board/chunk.tmj'
+const texturePath = '/textures/erick-etchebeur-transparent.webp'
 
 const canvasHook = {
-  app: new Application({background: '#1099bb', antialias: true}),
-  mounted() {
-    this.el.appendChild(this.app.view);
+  app: new Application({ background: '#1099bb', antialias: true }),
+  async mounted () {
+    this.el.appendChild(this.app.view)
     this.el.tabIndex = -1
     this.el.focus()
 
-    const canvas_center = {x: this.app.renderer.width / 2, y: this.app.renderer.height / 2}
-    let players = {}
+    const texture = await Texture.fromURL(texturePath).then((texture) => { return texture })
 
-    this.handleEvent("update-player-position", (data) => {
+    const canvasCenter = { x: this.app.renderer.width / 2, y: this.app.renderer.height / 2 }
+    const board = await loadBoard(boardPath)
+    const players = {}
+
+    const boardContainer = createContainer('board')
+    this.app.stage.addChild(boardContainer)
+
+    const playersContainer = createContainer('players')
+    this.app.stage.addChild(playersContainer)
+
+    const treesContainer = createContainer('trees')
+    this.app.stage.addChild(treesContainer)
+
+    renderBoard(boardContainer, texture, board.layers[0])
+    renderBoard(treesContainer, texture, board.layers[1])
+
+    this.handleEvent('update-player-position', (data) => {
       const sprite = players[data.name]
 
-      if(sprite) {
-        sprite.x = canvas_center.x + data.x
-        sprite.y = canvas_center.y + data.y
+      if (sprite) {
+        sprite.x = canvasCenter.x + data.x
+        sprite.y = canvasCenter.y + data.y
       } else {
-        const sprite = Sprite.from('https://pixijs.com/assets/bunny.png');
-        sprite.x = canvas_center.x + data.x
-        sprite.y = canvas_center.y + data.y
-        players[data.name] = sprite;
-        this.app.stage.addChild(sprite);
+        const sprite = Sprite.from('https://pixijs.com/assets/bunny.png')
+        sprite.x = canvasCenter.x + data.x
+        sprite.y = canvasCenter.y + data.y
+        players[data.name] = sprite
+        playersContainer.addChild(sprite)
       }
     })
 
-    this.handleEvent("delete-player", (data) => {
+    this.handleEvent('delete-player', (data) => {
       const sprite = players[data.name]
-      if(sprite) {
-        this.app.stage.removeChild(sprite)
+      if (sprite) {
+        playersContainer.removeChild(sprite)
         delete players[data.name]
       }
     })
   }
+}
+
+const loadBoard = (pathStr) => {
+  return fetch(pathStr).then((response) => { return response.json() })
+}
+
+const createContainer = (name) => {
+  const container = new Container()
+  container.name = name
+  return container
+}
+
+const renderBoard = (container, texture, layer) => {
+  const decodedBinaryString = atob(layer.data)
+  const byteArray = new Uint8Array(decodedBinaryString.length)
+
+  for (let i = 0; i < decodedBinaryString.length; i++) {
+    byteArray[i] = decodedBinaryString.charCodeAt(i)
+  }
+
+  // 2. Convert the bytes into Int32 values
+  const int32Array = new Int32Array(byteArray.buffer)
+  let index = 0
+  for (let y = 0; y < 20; y++) {
+    for (let x = 0; x < 25; x++) {
+      const position = int32Array[index]
+      if (position > 0) {
+        const cordX = x * 32
+        const cordY = y * 32
+        const ntexture = texture.clone()
+        ntexture.frame = textureFrame(position)
+
+        const sprite = Sprite.from(ntexture)
+        sprite.x = cordX
+        sprite.y = cordY
+        container.addChild(sprite)
+      }
+      index++
+    }
+  }
+}
+
+const textureFrame = (position) => {
+  const size = 32
+  const width = 14
+  const y = Math.floor(position / width)
+  const x = position % width - 1
+  console.log(y, x)
+  return new Rectangle(x * size, y * size, size, size)
 }
 
 export default { Hook: canvasHook }
